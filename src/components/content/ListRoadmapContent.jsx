dfa97aabb2361201cbf4e22d3663ae25c2ad8924
daffa <daffarobani551@gmail.com>
2024-12-16 23:03:37 +0700

    roadmap

--- src/components/content/ListRoadmapContent.jsx
+++ src/components/content/ListRoadmapContent.jsx
@@ -46,59 +46,59 @@ export function ListRoadmapContent({id}) {
                 ) : (
                     data?.data != null && (
 
-                 <ContentDistance>
+                        <ContentDistance>
 
-                     <HeaderContent
-                         title={data?.data.title}
-                         type={data?.data.type}
-                         date={data?.data.date}
-                         goal_level={data?.data.goal_level}
-                         knowledge_level={data?.data.knowledge_level}
+                            <HeaderContent
+                                title={data?.data.title}
+                                type={data?.data.type}
+                                date={data?.data.date}
+                                goal_level={data?.data.goal_level}
+                                knowledge_level={data?.data.knowledge_level}
 
-                     />
-                     {data?.data.roadmap.map((item, i) => (
-                         <Sheet key={item.id}>
-                             <SheetTrigger>
-                                 <div
-                                     className="flex justify-start w-full p-4 gap-4 rounded-lg bg-white border-accent border-2 hover:bg-accent cursor-pointer">
-                                     <div className="h-auto w-[1px] bg-black"></div>
-                                     <div className="flex flex-col items-start gap-3">
-                                         <h1>{`${i + 1}. ${item.title}`}</h1>
-                                         <p className="text-start lg:text-center">{item.desc}</p>
-                                     </div>
-                                 </div>
-                             </SheetTrigger>
-                             <SheetContent className="w-[400px] sm:w-[540px]">
-                                 <SheetHeader>
-                                     <SheetTitle>{item.title}</SheetTitle>
-                                     <SheetDescription>
-                                         {`${item.desc} ${item.detail_desc}`}
-                                     </SheetDescription>
-                                     <SheetDescription>
-                                         <h2 className="text-md mb-5">
-                                             Visit the following resources to learn more:
-                                         </h2>
-                                         <hr/>
-                                     </SheetDescription>
-                                     <SheetDescription>
-                                         {item.references.map((ref) => (
-                                             <div
-                                                 key={ref.id}
-                                                 className="flex flex-row items-center gap-5"
-                                             >
-                                                 <div className="p-2 text-sm bg-primary rounded text-white">
-                                                     {ref.type}
-                                                 </div>
-                                                 <p className="text-sm">{ref.content}</p>
-                                             </div>
-                                         ))}
-                                     </SheetDescription>
-                                 </SheetHeader>
-                             </SheetContent>
-                         </Sheet>
-                     ))}
+                            />
+                            {data?.data.roadmap.map((item, i) => (
+                                <Sheet key={item.id}>
+                                    <SheetTrigger>
+                                        <div
+                                            className="flex justify-start w-full p-4 gap-4 rounded-lg bg-white border-accent border-2 hover:bg-accent cursor-pointer">
+                                            <div className="h-auto w-[1px] bg-black"></div>
+                                            <div className="flex flex-col items-start gap-3">
+                                                <h1>{`${i + 1}. ${item.title}`}</h1>
+                                                <p className="text-start lg:text-center">{item.desc}</p>
+                                            </div>
+                                        </div>
+                                    </SheetTrigger>
+                                    <SheetContent className="w-[400px] sm:w-[540px]">
+                                        <SheetHeader>
+                                            <SheetTitle>{item.title}</SheetTitle>
+                                            <SheetDescription>
+                                                {`${item.desc} ${item.detail_desc}`}
+                                            </SheetDescription>
+                                            <SheetDescription>
+                                                <h2 className="text-md mb-5">
+                                                    Visit the following resources to learn more:
+                                                </h2>
+                                                <hr/>
+                                            </SheetDescription>
+                                            <SheetDescription className={'flex flex-col gap-2'}>
+                                                {item.references.map((ref) => (
+                                                    <div
+                                                        key={ref.id}
+                                                        className="flex flex-row items-center gap-5"
+                                                    >
+                                                        <div className="p-2 text-sm bg-primary rounded text-white">
+                                                            {ref.type}
+                                                        </div>
+                                                        <p className="text-sm">{ref.content}</p>
+                                                    </div>
+                                                ))}
+                                            </SheetDescription>
+                                        </SheetHeader>
+                                    </SheetContent>
+                                </Sheet>
+                            ))}
 
-                 </ContentDistance>
+                        </ContentDistance>
                     )
                 )}
             </div>
